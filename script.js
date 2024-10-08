66bcdd9512ddcd7138045b7cfe4a7a1dfc007db1
Arley Pereira <arleyedfisica@gmail.com>
2024-10-08 17:46:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,3 +40,30 @@ btnFechar.forEach(button => {
 
 
 /* FINAL DO POP UP */
+
+
+const popups = document.querySelectorAll('.popup');
+const closeButtons = document.querySelectorAll('.close');
+let scrollPosition;
+
+// Abrindo o pop-up ao clicar na imagem
+document.querySelectorAll('.slide a').forEach(anchor => {
+    anchor.onclick = (event) => {
+        event.preventDefault(); // Impede o comportamento padrão do link
+        scrollPosition = window.scrollY; // Salva a posição de rolagem
+        const popupId = anchor.getAttribute('href'); // Obtém o ID do pop-up
+        document.querySelector(popupId).style.display = 'block'; // Exibe o pop-up
+        document.body.style.overflow = 'hidden'; // Desabilita a rolagem
+    };
+});
+
+// Fechando o pop-up ao clicar no botão de fechar
+closeButtons.forEach(button => {
+    button.onclick = (event) => {
+        event.preventDefault(); // Impede o comportamento padrão do link
+        const popup = button.closest('.popup');
+        popup.style.display = 'none'; // Esconde o pop-up
+        document.body.style.overflow = ''; // Restaura a rolagem
+        window.scrollTo(0, scrollPosition); // Restaura a posição de rolagem
+    };
+});
